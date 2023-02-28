b537d86eb0faf8f817eb520e212efee2e68faa91
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2023-02-28 22:38:35 +0500

    lesson-42

--- js/script.js
+++ js/script.js
@@ -1,160 +1,152 @@
-// const form = document.querySelector('.form');
-
-// const nameInput = document.querySelector('#name');
-// const priceInput = document.querySelector('#price');
-// const itemId = document.querySelector('#itemId');
-
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-
-//     if (itemId.value) {
-//         item = JSON.parse(localStorage.getItem(itemId.value));
-//         item.name = nameInput.value;
-//         item.price = priceInput.value;
-
-//     } else {
-//         item = {
-//             name: nameInput.value,
-//             price: priceInput.value
-//         }
-//         itemId.value = `item ${Date.now()}`;
-//     }
-
-//     localStorage.setItem(itemId.value, JSON.stringify(item));
-//     nameInput.value = "";
-//     priceInput.value = "";
-//     itemId.value = "";
-
-//     renderList();
-// });
-// const itemList = document.querySelector('.itemList');
-
-
-// function renderList() {
-//     itemList.innerHTML = '';
-//     let totalPrice = 0;
-//     for (const key in localStorage) {
-//         if (localStorage.hasOwnProperty(key)) {
-//             const item = JSON.parse(localStorage.getItem(key));
-//             console.log(item);
-//             const list = document.createElement('li');
-//             list.innerHTML = `Name: ${item.name} Price: ${item.price}`;
-//             itemList.appendChild(list);
-//             const editButton = document.createElement('button');
-//             editButton.innerHTML = 'Edit';
-//             list.appendChild(editButton)
-//             editButton.addEventListener('click', () => {
-//                 nameInput.value = item.name;
-//                 priceInput.value = item.price;
-//                 itemId.value = key;
-//             });
-//             const deleteButton = document.createElement('button');
-//             deleteButton.innerHTML = 'Delete';
-//             list.appendChild(deleteButton);
-//             deleteButton.addEventListener('click', () => {
-//                 localStorage.removeItem(key);
-//                 renderList();
-//             })
-//             totalPrice = totalPrice + Number(item.price);
-//             console.log(totalPrice);
-//         }
-
-//     }
-// }
-// renderList();
-
-
-// const myFragment = document.createDocumentFragment();
-// const container = document.getElementById('countries');
-
-// const text1 = document.createElement('p');
-// const text2 = document.createElement('p');
-// const text3 = document.createElement('p');
-
-
-
-// myFragment.appendChild(text1);
-// myFragment.appendChild(text2);
-// myFragment.appendChild(text3);
-
-// container.appendChild(myFragment);
-
-// console.log('Alert tugasa chiqaman');
-// alert('Hello World');
-
-// const text = document.querySelector('.text');
 // const btn = document.querySelector('.btn');
+
+// const input = document.querySelector('.input');
+
+// const countryWrapper = document.querySelector('#countries');
 
 // btn.addEventListener('click', () => {
 
-//     setTimeout(() => {
-//         text.style.color = 'red';
+//     const name = input.value.toLowerCase();
+//     const url = `https://restcountries.com/v3.1/name/${name}`;
 
-//     }, 2000);
+//     countryWrapper.innerHTML = '';
 
-//     text.innerText = 'Hello World';
+//     fetch(url).then((response) => {
+//         if (!response.ok) {
+//             throw new Error('Ishlamayapti');
+//         }
+//         return response.json();
+//     }).then((data) => {
+//         data.map((country) => {
+//             const countryElement = document.createElement('div');
+//             const flagElement = document.createElement('img');
+//             const countryName = document.createElement('h3');
+//             const populationElement = document.createElement('span');
+//             flagElement.src = country.flags.png;
+//             flagElement.alt = country.name.common;
+//             countryName.innerText = country.name.common;
+//             countryElement.appendChild(flagElement);
+//             populationElement.innerText = country.population;
+//             countryElement.appendChild(countryName);
+//             countryElement.appendChild(populationElement);
+//             countryWrapper.appendChild(countryElement);
 
+//             console.log(country);
+//         })
+
+//     }).catch((error) => console.error('Nimadir xato ketgan API ga qara', error));
+
+//     input.value = '';
 // })
 
-// let pizza;
 
-// function orderPizza() {
-//     console.log('Order Pizza');
 
-//     setTimeout(() => {
-//         pizza = 'My Pizza';
-//     }, 3000);
 
-//     console.log('Pizza was ordered');
+
+
+
+// function myFun(number) {
+
+//     return function myInnerFun(param) {
+//         return number * param;
+//     }
 // }
 
-// orderPizza(); //
-// // 'Order Pizza'
+// const double = myFun(2);
 
-// // My pizza hozircha qayerdadir turadi
-// // Pizza was ordered
-
-// // Eat undefined
+// console.log(double(10));
 
 
-// console.log(`Eat ${pizza}`);
+// function myName(name, callback) {
+//     const greeting = name + callback();
+//     return greeting;
+// }
 
-// setTimeout(() => {
-//     console.log(pizza);
-// }, 2500);
+// function sayGreeting() {
+//     return 'Hello';
+// }
+
+// console.log(myName('Muhammadrasul ', sayGreeting));
 
 
-const name = 'uzbekistan';
-const url = `https://restcountries.com/v3.1/name/${name}`;
 
-const countryWrapper = document.querySelector('#countries');
+const apiKey = 'c1ba5d7054ad4225561ffacb783be3c6';
+const baseUrl = 'https://api.themoviedb.org/3/';
 
-fetch(url).then((response) => {
-    if (!response.ok) {
-        throw new Error('Ishlamayapti')
+const imagesUrl = 'https://image.tmdb.org/t/p/w500';
+
+
+const fragment = document.createDocumentFragment();
+
+
+const loader_container = document.querySelector('.loader-container');
+const loader = document.createElement('div');
+loader.classList.add('loader');
+
+loader_container.appendChild(loader);
+
+
+async function myAsyncFunction() {
+
+    const searchTerm = document.querySelector('#search').value;
+
+    let url;
+
+    if (searchTerm) {
+        url = `${baseUrl}search/movie?api_key=${apiKey}&query=${searchTerm}`;
+    } else {
+        url = `${baseUrl}discover/movie?api_key=${apiKey}`;
     }
-    return response.json();
-}).then((data) => {
-    data.map((country) => {
 
-        const countryElement = document.createElement('div');
-        const flagElement = document.createElement('img');
-        const countryName = document.createElement('h3');
-        const populationElement = document.createElement('span');
-        flagElement.src = country.flags.png;
-        flagElement.alt = country.name.common;
-        countryName.innerText = country.name.common;
-        countryElement.appendChild(flagElement);
-        populationElement.innerText = country.population;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
 
-        countryElement.appendChild(countryName);
-        countryElement.appendChild(populationElement);
-        countryWrapper.appendChild(countryElement);
-        console.log(country);
+        const movie_container = document.querySelector('.movie-container');
 
-    })
+        movie_container.innerHTML = '';
 
-}).catch((error) => console.error('Nimadir xato ketgan API ga qara', error));
+        data.results.map((movie) => {
+            const movieCard = document.createElement('div');
+            movieCard.classList.add('movie-card');
+
+
+            const movieImage = document.createElement('img');
+            movieImage.src = `${imagesUrl}${movie.backdrop_path}`;
+            movieImage.alt = movie.title;
+
+            const movieTitle = document.createElement('h3');
+            movieTitle.textContent = movie.title;
+
+            const movieRelease = document.createElement('span');
+            movieRelease.textContent = `Released: ${movie.release_date}`;
+
+
+
+            fragment.appendChild(movieImage);
+            fragment.appendChild(movieTitle);
+            fragment.appendChild(movieRelease);
+            movieCard.appendChild(fragment);
+            movie_container.appendChild(movieCard);
+        })
+
+    } catch (error) {
+        console.error(error.message);
+    } finally {
+        const loader = document.querySelector('.loader');
+        if (loader) {
+            loader_container.remove();
+        }
+    }
+}
+
+const searchInput = document.querySelector('#search')
+
+searchInput.addEventListener('input', () => {
+    myAsyncFunction();
+})
+
+myAsyncFunction();
 
 
 
